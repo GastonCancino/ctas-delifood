77d7d4418c393533b6b7faaf94509b047a008fb6
Gaston Cancino <guido.gaston@gmail.com>
2017-03-10 19:13:43 -0500

    commit viernes 10/03/2017

--- controlador/controladorRegPedidoAdmin.js
+++ controlador/controladorRegPedidoAdmin.js
@@ -10,31 +10,37 @@ controlador.tomarPedido = function(req, res){
 		msj2 : JSON.stringify(req.body, null, 2)
 	};
 
-	console.log("->" + JSON.stringify(req.body, null, 2));
+	//console.log("--->" + JSON.stringify(req.body, null, 2));
 	//console.log("-->" + res.json(req.body));
 	//console.log("--->" + req.body);
 
 	var jsonFormInputs = JSON.stringify(req.body, null, 2); //res.json(req.body); //
-	
+	var parseFormInputs = JSON.parse(jsonFormInputs);
+
 	var obj = [JSON.parse(jsonFormInputs)]; // [JSON.parse(jsonFormInputs)];
 	var keys = Object.keys(obj);
 	var valor;
 
-	for(var i = 0; i < keys.length; i++){
-		valor = obj[keys[i]];
-		console.log(obj[keys[i]]);
-	}
+	//var registro = {
+		for(var i = 0; i < keys.length; i++){
+			valor = obj[keys[i]];
+			console.log(obj[keys[i]]);
+		}
+	//}
 
-	/*
+	
 	// obtener los keys o atributos de un objeto json o arreglo.
 
 	// Forma 1:
 	var parseFormInputs = JSON.parse(jsonFormInputs);
-	var parseFormInputs = JSON.parse(jsonFormInputs);
 
 	for(var atr in parseFormInputs){
-		console.log(atr +" : " + parseFormInputs.atr);
+		console.log(atr);
+		if(atr == "id_carta_x_fecha"){
+			console.log("ok");
+		}
 	}
+	/*
 
 	// Forma 2:
 	var objetoInp = [JSON.parse(parseFormInputs)];
@@ -43,6 +49,14 @@ controlador.tomarPedido = function(req, res){
 		console.log(key);
 	}
 	*/
+
+	/*modeloRegPedidoAdmin.grabar(registro, function(err){
+		if(err){
+			console.log("Error no se pudo grabar el pedido, error: " + err);
+		}else{
+
+		}
+	});*/
 
 
 	res.render('ver-pedido-recuperado', datos);
